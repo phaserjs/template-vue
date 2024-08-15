ae3e046725ba55ac6515124e77fac614f0bca33a
Andres <costandres8@gmail.com>
2024-08-15 15:18:22 -0400

    fix alias path for vitejs in dev & prod env

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
+          '@': fileURLToPath(new URL('../src', import.meta.url))
         }
       },
     logLevel: 'warning',
